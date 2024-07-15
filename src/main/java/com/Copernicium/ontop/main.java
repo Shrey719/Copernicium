97ae25424bac012211632409b90d2db8797d14e1
sexmastercc <oscarlin@edu.schools.us.kg>
2024-07-15 13:42:04 -0400

    update example mode java thingy

--- src/main/java/com/Copernicium/ontop/main.java
+++ src/main/java/com/Copernicium/ontop/main.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.Copernicium.ontop;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = main.MODID, name = main.NAME, version = main.VERSION)
+public class main
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "copernicium";
+    public static final String NAME = "Copernicium";
+    public static final String VERSION = "1.0.0-alpha";
 
     private static Logger logger;
 
